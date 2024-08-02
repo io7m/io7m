4d248358af13457415f69d67510dfdebea70be10
Mark Raynsford <code@io7m.com>
2024-08-02 22:09:22 +0000

    Update README.

--- readme/make.sh
+++ readme/make.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 inkscape --export-type=png -o readme.png readme-overlay.svg || exit 1
-convert readme.png readme.jpg || exit 1
+magick readme.png readme.jpg || exit 1
 mv readme.jpg ../ || exit 1
